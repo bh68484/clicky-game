c02bb2d4993b900637e44e8fcb39671e698e544d
Brandon Harrington <bh68484@gmail.com>
2018-06-06 13:38:45 -0400

    Added jumbotron component.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Jumbotron from "./components/Jumbotron";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <Jumbotron />
+  </div>
+);
+
+export default App;
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+  <div className="jumbotron">
+    <h3>Clicky Game!</h3>
+    <p>
+      Click on an image to earn points, but don't click on any more than once!
+    </p>
+  </div>
+);
+
+export default Jumbotron;
